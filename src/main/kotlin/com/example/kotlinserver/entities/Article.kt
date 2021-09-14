68cb450d810cd4e983aec2e5ef837fbbcdb682ac
s2moon98 <s2moon98@github.com>
2021-09-14 11:17:22 +0900

    fix extensions

--- src/main/kotlin/com/example/kotlinserver/entities/Article.kt
+++ src/main/kotlin/com/example/kotlinserver/entities/Article.kt
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.ManyToOne
 
+import com.example.kotlinserver.extensions.toSlug
+
 @Entity
 class Article(
     var title: String,
@@ -13,5 +15,6 @@ class Article(
     var content: String,
     @ManyToOne var autor: User,
     var addedAt: LocalDateTime = LocalDateTime.now(),
-    @Id @GeneratedValue var id: Long? = null
+    @Id @GeneratedValue var id: Long? = null,
+    var slug: String = title.toSlug()
 )
